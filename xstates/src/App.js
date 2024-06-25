a3c920196e83935e8bafbe7fc3f31a8304e046e2
kondurusai <kondurusai863@gmail.com>
2024-06-26 00:23:31 +0530

    xstates done

---
+++ xstates/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import States from "./component/states";
+// import SelectOtherProps from "./component/muiselect";
+function App() {
+  return (
+    <div>
+      <States />
+    </div>
+  );
+}
+
+export default App;
---
+++ xstates/src/component/states.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+const States = () => {
+  const [statedis, setStatedis] = useState(true);
+  const [citydis, setCitdis] = useState(true);
+  const [final, setFinal] = useState({
+    country: "",
+    state: "",
+    city: "",
+  });
+  const [country, setCountry] = useState([]);
+  const [states, setStates] = useState([]);
+  const [citites, setCitites] = useState([]);
+  useEffect(() => {
+    let res = async () => {
+      try {
+        let response = await fetch(
+          "https://crio-location-selector.onrender.com/countries"
+        );
+        let data = await response.json();
+        console.log(data, "this is countries");
+        setCountry(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    res();
+  }, []);
+  useEffect(() => {
+    let res = async () => {
+      try {
+        let response = await fetch(
+          `https://crio-location-selector.onrender.com/country=${final.country}/states`
+        );
+        let data = await response.json();
+        console.log(data, "this is state data");
+
+        setStates(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    res();
+  }, [statedis]);
+  useEffect(() => {
+    let res = async () => {
+      try {
+        let response = await fetch(
+          ` https://crio-location-selector.onrender.com/country=${final.country}/state=${final.state}/cities`
+        );
+        let data = await response.json();
+        console.log(data, "this is state data");
+        setCitites(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    res();
+  }, [citydis]);
+
+  const onchange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setFinal({ ...final, [name]: value });
+    if (name === "country") {
+      setStatedis(false);
+    } else if (name === "state") {
+      setCitdis(false);
+    }
+  };
+  console.log(final);
+  console.log(statedis, "value of this one");
+  return (
+    <div>
+      <h1>Select Location</h1>
+      <div>
+        <select name="country" onChange={onchange}>
+          {country.map((val) => (
+            <option value={val}>{val}</option>
+          ))}
+        </select>
+        <select name="state" disabled={statedis} onChange={onchange}>
+          {states.map((val) => (
+            <option value={val}>{val}</option>
+          ))}
+        </select>
+        <select name="city" disabled={citydis} onChange={onchange}>
+          {citites.map((val) => (
+            <option value={val}>{val}</option>
+          ))}
+        </select>
+      </div>
+      {final.country && final.state && final.city ? (
+        <h1>
+          You Selected {final.city},{final.state},{final.country}
+        </h1>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default States;
